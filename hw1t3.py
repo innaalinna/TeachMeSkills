41bca1cfc46f12d24e74bd8ef7951ccb32c9f365
innaalinna <innaalexvic@gmail.com>
2019-06-14 16:40:57 +0300

    hw1 tasks

--- hw1t3.py
+++ hw1t3.py
@@ -1,16 +1,12 @@
 print("Please enter how much numbers do you want:")
 n = int(input())
-numList = []
 i = 0
-ord = []
 resPart1 = ''
 resPart2=''
-cnt = 0
 
 while i<n:
     print("Please enter number:")
     num = int(input())
-    numList.append(num)
     if num != 0:
         resPart1 += str(num)
     else:
