adf99085338bc3a86b635dd482c55e83a83d8d3e
innaalinna <innaalexvic@gmail.com>
2019-06-14 19:20:46 +0300

    hw1 tasks

--- hw1t4.py
+++ hw1t4.py
@@ -4,15 +4,14 @@ num = int(input())
 print("Please enter X:")
 x = float(input())
 #x = 3.14
-sign, i, n, nFact = 1.0, 0, 0, 1.0
+sign, i, n, nFact = -1.0, 0, 0, 1.0
 res=0.0
 while i<num:
-    if i%2 != 0 and i != 0 :
-        sign = -1.0
+
     for i in range(1,n+1):
         nFact = nFact*i
 
-    res += sign*(x**n)/nFact
+    res += (sign**i)*(x**n)/nFact
 
     i+=1
     n+=2
